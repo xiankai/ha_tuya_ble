75f3472c403c77281bfbb8ae2dd55858f0465f52
KJ <xiankai@gmail.com>
2026-05-24 22:06:26 +0900

    Add new Fingerbot ID 'HYFB0001' to mappings

--- custom_components/tuya_ble/select.py
+++ custom_components/tuya_ble/select.py
@@ -144,9 +144,16 @@ mapping: dict[str, TuyaBLECategorySelectMapping] = {
                 ],
             ),
             **dict.fromkeys(
-                ["ltak7e1p", "y6kttvd6", "yrnk7mnn",
-                    "nvr2rocq", "bnt7wajf", "rvdceqjh",
-                    "5xhbk964"],  # Fingerbot
+                [
+                    "ltak7e1p",
+                    "y6kttvd6",
+                    "yrnk7mnn",
+                    "nvr2rocq",
+                    "bnt7wajf",
+                    "rvdceqjh",
+                    "5xhbk964",
+                    "HYFB0001",
+                ],  # Fingerbot
                 [
                     TuyaBLEFingerbotModeMapping(dp_id=8),
                 ],
